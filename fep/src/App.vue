22b6e281ce250d4b36971c23c7ff30611da5b9ff
Arno <39265533+DeviousWolf4@users.noreply.github.com>
2019-05-16 10:58:14 +0200

    fep_oef1_16/05

--- fep/src/App.vue
+++ fep/src/App.vue
@@ -1,34 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/register">Register</router-link>
-      <router-link to="/login">Login</router-link>
-      <router-link to="/account">Register</router-link>
-      <router-link to="/board">Board</router-link>
-    </div>
+      <Header>
+      </Header>
     <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
+</script>
---
+++ fep/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="nav">
+      <router-link to="/">Home</router-link> | 
+      <router-link to="/register">Register</router-link> | 
+      <router-link to="/login">Login</router-link> | 
+      <router-link to="/account">Account</router-link> | 
+      <router-link to="/board">Board</router-link> | 
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+};
+</script>
