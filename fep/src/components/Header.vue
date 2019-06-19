75f39211943026f70761cfc415ec153eeb0dac9d
Koen Michiels <koen.michiels@telenet.be>
2019-06-19 20:33:06 +0200

    ex1

---
+++ fep/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="header">
+    <router-link to="/">Home</router-link>
+    <router-link to="/register">Register</router-link>
+    <router-link to="/login">login</router-link>
+    <router-link to="/account">Account</router-link>
+    <router-link to="/board">Board</router-link>
+  </div>
+ </template>
+
+
+<script>
+export default {
+  name: "Header",
+  };
+</script>
+
