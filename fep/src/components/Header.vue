c9ec55e096ccfbc26fe3a36e6e847a49178b2d14
Thibault Zenner <thibault.zenner@gmail.com>
2019-06-20 22:32:28 +0200

    fep_oef2_20/06/2019

--- fep/src/components/Header.vue
+++ fep/src/components/Header.vue
@@ -1,15 +1,146 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/register">Register</router-link> |
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/account">Account</router-link> |
-    <router-link to="/board">Board</router-link>
+    <router-link to="/"><h2>GRAIN <span class="other-color">&</span> MORTAR</h2></router-link>
+    <div id ="menuToggle">
+        <input type ="checkbox"/>
+        <span id ="bullets">&#8226;&#8226;&#8226;</span>
+          <div id="subnav">
+            <span id ="crossmark">x</span>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/register">Register</router-link> |
+            <router-link to="/login">Login</router-link> |
+            <router-link to="/account">Account</router-link> |
+            <router-link to="/board">Board</router-link>
+          </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import { scrollMixin} from '@/assets/scroll-mixin'
   export default {
     name: "Header",
+    data() {
+      return { mobileNavOpen: false};
+    },
+    mixins: [ scrollMixin],
+    methods: {
+      clickOpen: function () {
+        this.mobileNavOpen = true;
+      }
+    }
   };
 </script>
+
+<style>
+  body{
+    margin: 0;
+    padding: 0;
+  }
+  h2{
+    margin-left: 1rem;
+  }
+  #nav{
+    font-family: sans-serif;
+    color: white;
+    background-color: black;
+    padding-top: 0.25rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+  #subnav{
+    margin-right: 2rem;
+    justify-content: flex-end;
+  }
+  a{
+    color: white;
+    margin-left: 1rem;
+    text-decoration: none;
+  }
+  .other-color{
+    color: white;
+  }
+  input {
+    display:none;
+  }
+  #bullets,#crossmark{
+    display: none;
+  }
+
+  @media only screen and (max-width: 740px) {
+    #subnav {
+      margin-right: 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      background: white;
+      width: 40vw;
+      padding-top: 1rem;
+      padding-bottom: 1rem;
+      margin-top: -3.75rem;
+      right: 1rem;
+      height: 100%;
+      z-index: 2;
+      transform-origin: 0% 0%;
+      transform:translate(100%,0);
+    }
+    #subnav a{
+      color: black;
+      padding-bottom: 1rem;
+
+    }
+    .other-color{
+      color: orange;
+    }
+    #bullets{
+      display: flex;
+      flex-direction: column;
+      text-align: right;
+      color: white;
+      font-size: 2rem;
+      margin-right: 2rem;
+    }
+    #crossmark{
+      display: flex;
+      flex-direction: column;
+      text-align: right;
+      color: black;
+      font-size: 2rem;
+      margin-right: 2rem;
+    }
+    #menuToggle
+    {
+      display: block;
+      position: absolute;
+      top: 25px;
+      right: 0px;
+
+      z-index: 1;
+
+      -webkit-user-select: none;
+      user-select: none;
+    }
+    #menuToggle input
+    {
+      display: block;
+      opacity: 0;
+      width: 40px;
+      height: 32px;
+      position: absolute;
+      top: -7px;
+      right: 1.5rem;
+
+      cursor: pointer;
+      z-index: 2;
+
+      -webkit-touch-callout: none;
+    }
+    #menuToggle input:checked~ #subnav
+    {
+      transform: none;
+      opacity: 1;
+    }
+  }
+</style>
